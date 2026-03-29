d37fc1e6dba42246a618cef3d307f1f13d41219f
Liam <git@liamsherwin.com>
2026-03-29 22:12:47 +1300

    Added command palette entrys

--- InterfaceConfig.gd
+++ InterfaceConfig.gd
@@ -3,5 +3,13 @@ static var config: Dictionary[String, Variant] = {
 	"save_ui_on_quit": true,
 	"default_ui_panel": UICore,
 	"window_popup_config": {},
-	"startup_notices": []
+	"startup_notices": [],
 }
+
+
+## static init
+static func _static_init() -> void:
+	(func ():
+		Interface.add_command_palette_entry(CommandPaletteEntry.new(Launcher.get_settings(), "Launcher"))
+		Interface.add_command_palette_entry(CommandPaletteEntry.new(Interface.get_settings(), "Interface"))
+	).call_deferred()
